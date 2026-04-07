049004104b8c0477cfb2c3a1d45653745cd7c008
Nguyen Nhat Thong <work.nguyennhatthong@gmail.com>
2026-04-07 22:49:53 +0700

    refactor(config): update prisma configs to the new path

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,7 +4,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export default defineConfig({
-    schema: 'src/infrastructure/database/prisma/schema.prisma',
+    schema: 'src/infrastructure/database/prisma/schemas',
     migrations: {
         path: 'src/infrastructure/database/prisma/migrations',
     },
