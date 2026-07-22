f1c9ea5bfa69e9530eee9a1e48cc620a17c8fa42
Nguyen Nhat Thong <work.nguyennhatthong@gmail.com>
2026-07-22 16:07:03 +0700

    fix(use case): fix misnamed file

--- src/application/application.module.ts
+++ src/application/application.module.ts
@@ -3,7 +3,7 @@ import SignUpUseCase from './use-cases/auth/sign-up.use-case';
 import { SignInUseCase } from './use-cases/auth/sign-in.use-case';
 import { SignOutUseCase } from './use-cases/auth/sign-out.use-case';
 import { RefreshTokenUseCase } from './use-cases/auth/refresh-token.use-case';
-import { SignOutAllUseCase } from './use-cases/auth/sign-out-all-session.dto';
+import { SignOutAllUseCase } from './use-cases/auth/sign-out-all-session.use-case';
 import { GetCurrentUserUseCase } from './use-cases/user/get-current-user.use-case';
 import { GetActiveSessionUseCase } from './use-cases/session/get-active-session.use-case';
 
